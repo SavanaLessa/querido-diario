017f01b320575d544df94cd528023559832d2722
Savana Lessa <savanayasmim@gmail.com>
2020-06-01 20:39:55 -0300

    fix redis

--- app/code/app/Http/Controllers/RegistroCriseController.php
+++ app/code/app/Http/Controllers/RegistroCriseController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Services\RegistroService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class RegistroCriseController extends Controller
 {
@@ -23,7 +25,21 @@ class RegistroCriseController extends Controller
 
     public function index(){
 
-        $registros = $this->registroService->getRegistros();
+        $cacheKey = 'REGISTROS_CRISES_USUARIO_'.auth()->user()->id;
+
+        if (Cache::has($cacheKey)){
+
+            $registros = Cache::get($cacheKey);
+
+        }else{
+
+            $registros = $this->registroService->getRegistros();
+
+            if($registros->isNotEmpty()){
+
+                Cache::add($cacheKey, $registros, Carbon::now()->addHours(2));
+            }
+        }
 
         return view('registros.index')->with([
             'registros' => $registros
@@ -38,6 +54,12 @@ class RegistroCriseController extends Controller
     public function store(Request $request){
 
         $retorno = $this->registroService->store($request->all());
+
+        $cacheKey = 'REGISTROS_CRISES_USUARIO_'.auth()->user()->id;
+
+        if(Cache::has($cacheKey)){
+            Cache::forget($cacheKey);
+        }
 
         if(!isset($retorno['error'])){
             return redirect()->route('registro.crises.index')->with('success', 'Registro cadastrado com sucesso.');
