2444d3f328ec6369dbcc5b80ef7222d16dda1f10
Savana Lessa <savanayasmim@gmail.com>
2020-06-22 17:40:58 -0300

    fix favicon

--- app/code/resources/views/layouts/layout.blade.php
+++ app/code/resources/views/layouts/layout.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon.png">
+    
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/img/Imagens.png')}}">
     <link rel="stylesheet" href="assets/css/pace.css">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Querido diário</title>
